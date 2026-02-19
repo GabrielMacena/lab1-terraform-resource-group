02da0ca2caef1feeb81fa48b94ba967caa5c700b
Gabriel Macena <76190163+GabrielMacena@users.noreply.github.com>
2026-02-19 09:42:01 -0300

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+  
+}
